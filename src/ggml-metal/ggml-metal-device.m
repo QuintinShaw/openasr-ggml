98cb1c47e3f394963e3a4f5dd7601fa792304c19
QuintinShaw <github@xyt.email>
2026-05-29 10:57:04 +0800

    perf(metal): default residency sets off without tensor api
    Signed-off-by: QuintinShaw <github@xyt.email>

--- src/ggml-metal/ggml-metal-device.m
+++ src/ggml-metal/ggml-metal-device.m
@@ -974,9 +974,13 @@ ggml_metal_device_t ggml_metal_device_init(int device) {
                 }
             }
 
-            dev->props.use_residency_sets = true;
+            dev->props.use_residency_sets = dev->props.has_tensor;
 #if defined(GGML_METAL_HAS_RESIDENCY_SETS)
-            dev->props.use_residency_sets = getenv("GGML_METAL_NO_RESIDENCY") == nil;
+            if (getenv("GGML_METAL_RESIDENCY_ENABLE") != NULL) {
+                dev->props.use_residency_sets = true;
+            } else if (getenv("GGML_METAL_NO_RESIDENCY") != NULL) {
+                dev->props.use_residency_sets = false;
+            }
 #endif
 
             dev->props.use_shared_buffers = dev->props.has_unified_memory;
